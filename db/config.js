3bcad4dfc73262d94f0a435206129f0617f37f8b
NazmulHasan3580 <n47712338@gmail.com>
2024-07-29 11:35:40 +0600

    class 9

---
+++ db/config.js
@@ -0,0 +1,5 @@
+const mongoose = require("mongoose")
+mongoose.connect('mongodb+srv://n47712338:7j8FfaD9T3d3HDro@cluster0.sxnh2fa.mongodb.net/reactnode?retryWrites=true&w=majority&appName=Cluster0')
+
+
+//this config file is just only for connec db
--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 const express = require('express')
-const { default: mongoose } = require('mongoose')
+const user = require('./db/user')
+require('./db/config')
 const app = express()
 
-const connectDB = async()=>{
-    mongoose.connect("mongodb+srv://n47712338:7j8FfaD9T3d3HDro@cluster0.sxnh2fa.mongodb.net/reactnode?retryWrites=true&w=majority&appName=Cluster0")
-    const userSchema = new mongoose.Schema({})
-    const user = mongoose.model('users',userSchema)
-    const data = await user.find()
-    console.log(data);
-
-}
-connectDB()
-
+app.use(express.json())
+app.post('/register', async(req, res)=>{
+    let data = new user(req.body)
+    const result =await data.save()
+    res.send(result)
+})
 
 app.listen(5000)
+
+
+
+
+
+
+
+
+// const connectDB = async()=>{
+//     mongoose.connect("mongodb+srv://n47712338:7j8FfaD9T3d3HDro@cluster0.sxnh2fa.mongodb.net/reactnode?retryWrites=true&w=majority&appName=Cluster0")
+//     const userSchema = new mongoose.Schema({})
+//     const user = mongoose.model('users',userSchema)
+//     const data = await user.find()
+//     console.log(data);
+
+// }
+// connectDB()
