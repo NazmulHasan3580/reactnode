92ff86a8cbe4b06b564848de8e96303a33314f59
NazmulHasan3580 <n47712338@gmail.com>
2024-07-30 11:59:54 +0600

    class 16

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
-const express = require('express')
-const user = require('./db/user')
-require('./db/config')
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const user = require("./db/user");
+require("./db/config");
+const app = express();
 
-app.use(express.json())
-app.post('/register', async(req, res)=>{
-    let data = new user(req.body)
-    const result =await data.save()
-    res.send(result)
-})
+app.use(cors());
 
-app.listen(5000)
+app.use(express.json());
+app.post("/register", async (req, res) => {
+  let data = new user(req.body);
+  let result = await data.save();
+  result = result.toObject();
+  delete result.password
+  res.send(result);
+});
 
+app.post("/login", async (req, res) => {
+  if (req.body.password && req.body.email) {
+    let data = await user.findOne(req.body).select("-password");
+    if (data) {
+      res.send(data);
+    } else {
+      res.send({ result: "No User Found" });
+    }
+  }else{
+    res.send({ result: "No User Found" });
+  }
+});
 
-
-
-
-
-
+app.listen(5000);
 
 // const connectDB = async()=>{
 //     mongoose.connect("mongodb+srv://n47712338:7j8FfaD9T3d3HDro@cluster0.sxnh2fa.mongodb.net/reactnode?retryWrites=true&w=majority&appName=Cluster0")
